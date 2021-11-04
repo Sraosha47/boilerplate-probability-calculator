bc90c0ad65e8c9eec85f2129e9bf3954f5ac4734
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2021-11-04 21:13:32 +0100

    Defined function to show balls drawn with test. Added aditional call of test_content to show balls are removed from the list contents

--- prob_test.py
+++ prob_test.py
@@ -3,6 +3,13 @@ def testing(test_class):
     hat1 = test_class(yellow=3, blue=2, green=6)
     
     def test_contents(test_class):
-        print(test_class.contents)
+        print("Contents:", test_class.contents)
    
+
+
+    def test_draw(test_class):
+        print("Drawn balls", test_class.draw(3))
+
+    test_contents(hat1)
+    test_draw(hat1)
     test_contents(hat1)
