e64a08216b09ab0f5d06e286431345a901aa0967
squoshy <70937539+Sraosha47@users.noreply.github.com>
2021-11-04 23:11:37 +0000

    Changed indentation and sorted the list returned by the draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,47 +1,42 @@
 import copy
 import random
-from prob_test import testing
 
 # Consider using the modules imported above.
 
 class Hat:
-    def __init__(self, **balls):
-        self.contents = list()
-        for colour, count in balls.items():
-            i = 0
-            while i < count:
-                self.contents.append(colour)
-                i += 1
+	def __init__(self, **balls):
+		self.contents = list()
+		for colour, count in balls.items():
+				i = 0
+				while i < count:
+						self.contents.append(colour)
+						i += 1
 
-    def draw(self, amount):
-        i = 0
-        drawn = list()
-        while i < amount:
-            drawn.append(self.contents.pop(random.randrange(len(self.contents)-1)))
-            i += 1
-        return(drawn)
+	def draw(self, amount):
+		i = 0
+		self.drawn = list()
+		while i < amount:
+				self.drawn.append(self.contents.pop(random.randrange(len(self.contents)-1)))
+				i += 1
+		self.drawn.sort()
+		return(self.drawn)
 
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    i = 0
-    count = 0
-    drawn = {}
-
-    print(expected_balls.items())
-    while i < num_experiments:
-        while drawn != expected_balls:
-            copyhat = copy.deepcopy(hat)
-            drawn_list = copyhat.draw(num_balls_drawn)
-            for item in drawn_list:
-                if list(expected_balls.keys()).count(item) != 0: 
-                    drawn.update({item : drawn_list.count(item)})
-                    print(drawn.items())
-            count += 1
-        i += 1
+	i = 0
+	count = 0
+	drawn = {}
+	while i < num_experiments:
+			while drawn != expected_balls:
+					copyhat = copy.deepcopy(hat)
+					drawn_list = copyhat.draw(num_balls_drawn)
+					for item in drawn_list:
+							if list(expected_balls.keys()).count(item) != 0: 
+									drawn.update({item : drawn_list.count(item)})
+					count += 1
+			i += 1
     
-    return(count/num_experiments)
+	return(count/num_experiments)
     
 
-
-testing(Hat, experiment)
