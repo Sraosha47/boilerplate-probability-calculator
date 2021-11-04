eeb966ec6d6e96588df250bd17d23bd7a40e5e7e
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2021-11-05 00:01:15 +0100

    wrote at test for the function experiment. but the probability isn't at all like the one expected in the test module

--- prob_test.py
+++ prob_test.py
@@ -1,6 +1,6 @@
-def testing(test_class):
+def testing(test_class, test_function):
     
-    hat1 = test_class(yellow=3, blue=2, green=6)
+    hat1 = test_class(red=5,blue=2)
     
     def test_contents(test_class):
         print("Contents:", test_class.contents)
@@ -8,8 +8,14 @@ def testing(test_class):
 
 
     def test_draw(test_class):
-        print("Drawn balls", test_class.draw(3))
+        print("Drawn balls", test_class.draw(2))
+        print(len(test_class.contents))
+
+    def test_experiment(test_function):
+        print( "The probability is:", test_function(hat=hat1, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000))
+        
 
     test_contents(hat1)
     test_draw(hat1)
     test_contents(hat1)
+    test_experiment(test_function)
